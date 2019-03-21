c79d842529b06063e5c3f0e8bf9bb28aba51577d
unknown <D:\FVV\work\maikl_popov@mail.ru>
2019-03-21 13:45:20 +0300

    add and edit files

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Post;
+use App\Http\Requests\StorePost as StorePostRequest;
+use Auth;
 
 class PostController extends Controller
 {
@@ -17,5 +19,14 @@ class PostController extends Controller
     public function create()
     {
         return view('posts.create');
+    }
+
+    public function store(StorePostRequest $request)
+    {
+        $data = $request->only('title', 'body');
+        $data['slug'] = str_slug($data['title']);
+        $data['user_id'] = Auth::user()->id;
+        $post = Post::create($data);
+        return redirect()->route('edit_post', ['id' => $post->id]);
     }
 }
